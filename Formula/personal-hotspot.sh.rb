eb5753531c86322a30b7e953d32519f4fadcf944
Tuk Bredsdorff <tukodeb@gmail.com>
2024-12-20 00:17:05 +0100

    Updated version

--- Formula/personal-hotspot.sh.rb
+++ Formula/personal-hotspot.sh.rb
@@ -1,8 +1,8 @@
 class PersonalHotspotSh < Formula
   desc "Script to connect to a personal hotspot on a Mac"
   homepage "https://github.com/tiktuk/personal-hotspot.git"
-  url "https://raw.githubusercontent.com/tiktuk/personal-hotspot/refs/tags/v0.1/personal-hotspot.sh"
-  sha256 "487c5fcc3dadd85dee7a7bdc0830bee07c9cbb72d4fc49969a3ceef522598621"
+  url "https://raw.githubusercontent.com/tiktuk/personal-hotspot/refs/tags/v0.1.1/personal-hotspot.sh"
+  sha256 "2a2cf7cbe25327289d62373b4b6d111ab16692fd637a98cab0c41a5e4a7acce8"
   license "MIT"
 
   def install
