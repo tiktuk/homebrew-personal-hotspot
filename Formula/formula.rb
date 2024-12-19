356db174523d08c18359f9b63e97ccc0f76e7377
Tuk Bredsdorff <tukodeb@gmail.com>
2024-12-19 23:58:32 +0100

    Removed comments from template

--- Formula/formula.rb
+++ Formula/formula.rb
@@ -2,11 +2,11 @@ class Myscript < Formula
   desc "Script to connect to a personal hotspot on a Mac"
   homepage "https://github.com/tiktuk/personal-hotspot.git"
   url "https://raw.githubusercontent.com/tiktuk/personal-hotspot/refs/tags/v0.1/personal-hotspot.sh"
-  sha256 "487c5fcc3dadd85dee7a7bdc0830bee07c9cbb72d4fc49969a3ceef522598621" # Calculate using `shasum -a 256` on the downloaded file
+  sha256 "487c5fcc3dadd85dee7a7bdc0830bee07c9cbb72d4fc49969a3ceef522598621"
   license "MIT"
 
   def install
-    bin.install "personal-hotspot.sh" # Ensure your script's name matches
+    bin.install "personal-hotspot.sh"
   end
 
   test do
